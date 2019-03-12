1098a5824b364d1190780f441563e5942bbf32cb
landcold7 <landcold7@gmail.com>
2019-03-12 12:57:01 +0800

    Pull transcript from cnn10 2019-03-12

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 git add --all
-git commit -v -m "Pull transcript from cnn10 $(shell date "+%Y-%m-%d")"
+git commit -v -m "Pull transcript from cnn10 $(date "+%Y-%m-%d")"
 eval `ssh-agent`
 ssh-add ~/.ssh/weak
 git push -u origin master
