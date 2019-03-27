a3d20e368a561c3901d7c341045a236d81da6329
landcold7 <landcold7@gmail.com>
2019-03-27 02:00:01 +0000

    Pull transcript from cnn10 2019-03-27

--- pull.py
+++ pull.py
@@ -18,7 +18,9 @@ def make_pdf(name, content):
 
 def get_html(url):
     req = requests.get(url)
-    if req.status_code == 404: exit(1)
+    if req.status_code == 404: 
+        print('Oops!! 404')
+        exit(1)
     html = req.text
     head = re.search('class="cnnBodyText">(.*)<\\/P>', html).groups()[0]
     body = re.search('class="cnnBodyText">(.*\n.*)<\\/div>', html).groups()[0]
@@ -35,11 +37,15 @@ def pull():
     today = str(date.today()).split('-')
     # script-2019-03-07.pdf
     fn = 'script-' + '-'.join(x for x in today) + '.pdf'
+    print('Pulling %s' % fn)
     fn = archives / fn
-    if check_file_exist(fn): exit(1)
+    if check_file_exist(fn): 
+        print('File already exist')
+        exit(1)
 
     partial = today[0][2:] + today[1] + '/' + today[2]
     url = 'http://transcripts.cnn.com/TRANSCRIPTS/' + partial + '/sn.01.html'
+    print('Url %s' % url)
     content = get_html(url)
     make_pdf(fn, content)
     print('Pulling script %s job done.' % fn)
